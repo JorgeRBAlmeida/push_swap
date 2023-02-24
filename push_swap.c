977d555a47d8c1b22f930ce61ba0f6ae184e366c
Jorge RB Almeida <jorgerodrigo.b.a@gmail.com>
2023-02-23 21:43:36 -0300

    feature: validate

--- push_swap.c
+++ push_swap.c
@@ -6,12 +6,59 @@
 /*   By: joalmeid <joalmeid@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 00:33:40 by joalmeid          #+#    #+#             */
-/*   Updated: 2023/02/23 01:09:17 by joalmeid         ###   ########.fr       */
+/*   Updated: 2023/02/23 21:42:28 by joalmeid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+int	is_arg_number(char *str)
+{
+	int	i;
+
+	i = 0;
+	while(str[i])
+	{
+		if (i == 0 && str[i] == '-')
+			i ++;
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i ++;
+	}
+	return (1);
+}
+
+int	validate_args(char **argv)
+{
+	int	i;
+	int	j;
+	
+	i= 1;
+	j = 1;
+	while (argv[i])
+	{
+		if (!is_arg_number(argv[i]) ||
+			((ft_atoi(argv[i]) > INT_MAX) || \
+				( ft_atoi(argv[i]) < INT_MIN)))
+			return (0);
+		j = i - 1;
+		while (i > 1 && j >= 1)
+		{
+			if (ft_atoi(argv[i]) == ft_atoi(argv[j]))
+				return (0);
+			j --;
+		}
+		i ++;
+	}
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
-	if (argc && argv[0])
-	return (0);
+	if (argc > 1)
+	{
+		if (!validate_args(argv))
+			ft_putstr_fd("Error\n", 1);
+	}
+		return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,15 +6,28 @@
 /*   By: joalmeid <joalmeid@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 00:33:33 by joalmeid          #+#    #+#             */
-/*   Updated: 2023/02/23 01:56:16 by joalmeid         ###   ########.fr       */
+/*   Updated: 2023/02/23 20:34:58 by joalmeid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	PUSH_SWAP_H
-# define	PUSH_SWAP_H
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# ifndef INT_MAX
+#  define INT_MAX 2147483647
+# endif
+# ifndef INT_MIN
+#  define INT_MIN -2147483648
+# endif
 
 # include <unistd.h>
 # include <stdlib.h>
-# include "libft.h"
+# include "libft/libft.h"
+
+typedef struct s_node {
+	int				index;
+	int				data;
+	struct s_node	*next;
+}			t_node;
 
 #endif
